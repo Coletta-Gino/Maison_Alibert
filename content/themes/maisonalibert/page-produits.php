60f62fa942abb208a935b2be73049c35ea819810
Coletta-Gino <gino3869.co@hotmail.com>
2022-10-31 16:31:45 +0100

    Styles Tests (Part. 2) + Add Products Page

--- content/themes/maisonalibert/page-produits.php
+++ content/themes/maisonalibert/page-produits.php
@@ -4,8 +4,128 @@
   <?php endwhile; endif; ?>
 
   <!-- Categories -->
-  <section class="categories">
-    <button class="selected">Tous</button>
+  <button class="sorting">sort</button>
+  <button class="filtering">filters</button>
+
+  <div class="container">
+    <article class="product">
+      <div class="product__image">
+        <img src="" alt="">
+      </div>
+
+      <div class="product__infos">
+        <h3>Product name</h3>
+
+        <div class="product__infos__colors">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+        <p class="product__infos__price">12,99 €</p>
+
+        <a href="#" class="view-more">View more</a>
+      </div>
+    </article>
+
+    <article class="product">
+      <div class="product__image">
+        <img src="" alt="">
+      </div>
+
+      <div class="product__infos">
+        <h3>Product name</h3>
+
+        <div class="product__infos__colors">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+        <p class="product__infos__price">12,99 €</p>
+
+        <a href="#" class="view-more">View more</a>
+      </div>
+    </article>
+
+    <article class="product">
+      <div class="product__image">
+        <img src="" alt="">
+      </div>
+
+      <div class="product__infos">
+        <h3>Product name</h3>
+
+        <div class="product__infos__colors">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+        <p class="product__infos__price">12,99 €</p>
+
+        <a href="#" class="view-more">View more</a>
+      </div>
+    </article>
+
+    <article class="product">
+      <div class="product__image">
+        <img src="" alt="">
+      </div>
+
+      <div class="product__infos">
+        <h3>Product name</h3>
+
+        <div class="product__infos__colors">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+        <p class="product__infos__price">12,99 €</p>
+
+        <a href="#" class="view-more">View more</a>
+      </div>
+    </article>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- <section class="categories">
+    <button class="selected">Tous</button> -->
 
     <?php
       // TODO => be careful to change the value of 'parent' = the id of the ephemerals category !!!
@@ -23,9 +143,9 @@
         echo '<button>' . $category . '</button>';
       }
     ?>
-  </section>
+  <!-- </section> -->
 
-  <div class="container">
+  <!-- <div class="container"> -->
     <?php  
       $args = [
         'post_type' => 'post',
@@ -39,5 +159,5 @@
     <?php if ($wpqueryArticles->have_posts()): while ($wpqueryArticles->have_posts()): $wpqueryArticles->the_post(); ?>
       <?php get_template_part('template-parts/products/ephemerals'); ?>
     <?php endwhile; endif; ?>    
-  </div>
+  <!-- </div> -->
 <?php get_footer(); ?>
--- content/themes/maisonalibert/template-parts/header/nav.php
+++ content/themes/maisonalibert/template-parts/header/nav.php
@@ -4,8 +4,8 @@
       <a href="<?php echo home_url(); ?>">
         <?php 
           // TODO => be careful to change the value of $attachment_id = the id of the logo's picture !!!
-          $image_attributes = wp_get_attachment_image_src(32, 'full');
-          $alt_text = get_post_meta(32, '_wp_attachment_image_alt', true);
+          $image_attributes = wp_get_attachment_image_src(37, 'full');
+          $alt_text = get_post_meta(37, '_wp_attachment_image_alt', true);
         ?>
   
         <img src="<?php echo $image_attributes[0]; ?>" alt="<?php echo $alt_text; ?>" />
