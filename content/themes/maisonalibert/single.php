3263b0568ec06f3f914b9b9b0232d40eaa9b4a12
Coletta-Gino <gino3869.co@hotmail.com>
2022-11-21 16:58:56 +0100

    Dynamization Tests On Products Page (Part. 4)

--- content/themes/maisonalibert/single.php
+++ content/themes/maisonalibert/single.php
@@ -1,122 +1,106 @@
 <!-- Product Details -->
 <?php get_header(); ?>
-  <section>
-    <div class="product-details">
-      <?php if (have_posts()): while (have_posts()): the_post(); ?>
-        <h1><?php the_title(); ?></h1>
-      <?php endwhile; endif; ?>
+  <section class="product-details">
+    <?php if (have_posts()): while (have_posts()): the_post(); ?>
+      <h1><?php the_title(); ?></h1>
+    <?php endwhile; endif; ?>
 
-      <div class="pictures">
-        <!-- <div class="prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></div> -->
-        <div class="main-picture">
-          <img src="" alt="">
-        </div>
-        <!-- <div class="next"><i class="fa fa-chevron-right" aria-hidden="true"></i></div> -->
+    <div class="pictures">
+      <?php the_content(); ?>
 
-        <div class="secondaries-pictures">
-          <?php echo do_shortcode('[sp_wpcarousel id="50"]'); ?>
-
-          <style>
-            .swiper-slide-active {
-              padding: 50px;
-              border: solid 5px red;
-            }
-          </style>
-
-          <div class="box"></div>
-          <div class="box"></div>
-          <div class="box"></div>
-          <div class="box"></div>
-          <div class="box"></div>
-          <div class="box"></div>
-        </div>
-      </div>
-
-      <div class="colors">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-
-      <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, sit?</p>
-
-      <p class="price">29,99€</p>
+      <?php // .wpcp-carousel-section .wpcp-single-item img {margin: 0 auto; max-width: 100%; height: auto; box-shadow: none; border-radius: 15px;}; ?>
     </div>
 
-    <div class="comments-area">
-      <div class="card">
-        <h2>average / 5</h2>
+    <div class="colors">
+      <p>Disponible en :</p>
+      
+      <div class="product__infos__colors">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
 
-        <div class="rating">
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star-half" aria-hidden="true"></i>
+    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, sit?</p>
+
+    <p class="price">29,99€</p>
+  </section>
+
+  <section class="comments-area">
+    <div class="card">
+      <h2>average / 5</h2>
+
+      <div class="rating">
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star-half" aria-hidden="true"></i>
+      </div>
+
+      <div class="total-reviews">
+        <h3>(number) reviews</h3>
+      </div>
+
+      <div class="reviews-by-stars">
+        <div class="five-stars">
+          <p>5 stars</p>
+          <div class="progress-bar"></div>
+          <p class="number-of-reviews">(number of 5 stars reviews)</p>
         </div>
 
-        <div class="total-reviews">
-          <h3>(number) reviews</h3>
+        <div class="four-stars">
+          <p>4 stars</p>
+          <div class="progress-bar"></div>
+          <p class="number-of-reviews">(number of 4 stars reviews)</p>
         </div>
 
-        <div class="reviews-by-stars">
-          <div class="five-stars">
-            <p>5 stars</p>
-            <div class="progress-bar"></div>
-            <p class="number-of-reviews">(number of 5 stars reviews)</p>
-          </div>
-
-          <div class="four-stars">
-            <p>4 stars</p>
-            <div class="progress-bar"></div>
-            <p class="number-of-reviews">(number of 4 stars reviews)</p>
-          </div>
-
-          <div class="three-stars">
-            <p>3 stars</p>
-            <div class="progress-bar"></div>
-            <p class="number-of-reviews">(number of 3 stars reviews)</p>
-          </div>
-
-          <div class="two-stars">
-            <p>2 stars</p>
-            <div class="progress-bar"></div>
-            <p class="number-of-reviews">(number of 2 stars reviews)</p>
-          </div>
-
-          <div class="one-star">
-            <p>1 star</p>
-            <div class="progress-bar"></div>
-            <p class="number-of-reviews">(number of 1 star reviews)</p>
-          </div>
+        <div class="three-stars">
+          <p>3 stars</p>
+          <div class="progress-bar"></div>
+          <p class="number-of-reviews">(number of 3 stars reviews)</p>
         </div>
 
-        <div class="add-review">
-          <h4>Write review here</h4>
-          <button type="submit">Submit review</button>
+        <div class="two-stars">
+          <p>2 stars</p>
+          <div class="progress-bar"></div>
+          <p class="number-of-reviews">(number of 2 stars reviews)</p>
         </div>
 
-        <div class="comments" style="color: #f0eae2; background-color: #1d616c;">
-          <div>
-            <div class="user-picture">
-              <img src="" alt="">
-            </div>
+        <div class="one-star">
+          <p>1 star</p>
+          <div class="progress-bar"></div>
+          <p class="number-of-reviews">(number of 1 star reviews)</p>
+        </div>
+      </div>
 
-            <p class="user-name">Author</p>
+      <div class="add-review">
+        <h4>Write review here</h4>
+        <button type="submit">Submit review</button>
+      </div>
 
-            <div class="user-rate">
-              <i class="fa fa-star" aria-hidden="true"></i>
-              <i class="fa fa-star" aria-hidden="true"></i>
-              <i class="fa fa-star" aria-hidden="true"></i>
-              <i class="fa fa-star" aria-hidden="true"></i>
-              <i class="fa fa-star-half" aria-hidden="true"></i>
-            </div>
-
-            <p class="user-review">User's comment</p>
-
-            <p class="review-date">On monday 14th, november 2022 16:23:42 AM</p>
+      <div class="comments" style="color: #f0eae2; background-color: #1d616c;">
+        <div>
+          <div class="user-picture">
+            <img src="" alt="">
           </div>
+
+          <p class="user-name">Author</p>
+
+          <div class="user-rate">
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star-half" aria-hidden="true"></i>
+          </div>
+
+          <p class="user-review">User's comment</p>
+
+          <p class="review-date">On monday 14th, november 2022 16:23:42 AM</p>
         </div>
       </div>
     </div>
--- content/themes/maisonalibert/template-parts/products/article.php
+++ content/themes/maisonalibert/template-parts/products/article.php
@@ -15,26 +15,26 @@
 
           <!-- <h3>Product name</h3> -->
           <?php if ( ! function_exists( 'maisonalibert_the_title' ) ) {
-	        function maisonalibert_the_title($class = 'product__infos__name', $link = TRUE) {
-		      $title_before = '<h3' . ' class="' . $class . '">';
-		      $title_after = '</h3>';
+	          function maisonalibert_the_title($class = 'product__infos__name', $link = TRUE) {
+		          $title_before = '<h3' . ' class="' . $class . '">';
+		          $title_after = '</h3>';
 
-		      if ($link === TRUE) {
-			    $output = the_title( sprintf( $title_before . '<a href="%s">', esc_url( get_permalink() ) ), '</a>' . $title_after );
-		      } 
+		          if ($link === TRUE) {
+			          $output = the_title( sprintf( $title_before . '<a href="%s">', esc_url( get_permalink() ) ), '</a>' . $title_after );
+		          } 
               else {
-			    $output = $title_before . get_the_title() . $title_after;
-		      }
+			          $output = $title_before . get_the_title() . $title_after;
+		          }
 
-		      echo $output;
-	        }
+		          echo $output;
+	          }
           } ?>
 
           <?php if ( is_single() ) : ?>
-		    <?php maisonalibert_the_title('product__infos__name', false); ?>
-		  <?php else : ?>
-		    <?php maisonalibert_the_title('product__infos__name'); ?>
-		  <?php endif; ?>
+		        <?php maisonalibert_the_title('product__infos__name', false); ?>
+		      <?php else : ?>
+		        <?php maisonalibert_the_title('product__infos__name'); ?>
+		      <?php endif; ?>
 
           <div class="product__infos__colors">
             <div></div>
@@ -46,7 +46,5 @@
           </div>
 
           <p class="product__infos__price">12,99 €</p>
-
-          <!-- <a href="#" class="view-more">View more</a> -->
         </div>
       </article>
