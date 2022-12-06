734162af9edbae94d99eb7942e63f81d4515b0eb
Coletta-Gino <gino3869.co@hotmail.com>
2022-12-06 16:45:41 +0100

    Comments Area (Part. 3)

--- content/themes/maisonalibert/comments.php
+++ content/themes/maisonalibert/comments.php
@@ -79,26 +79,33 @@
   </section> -->
 
   <section id="comments" class="comments-area">
+    <h2>Avis</h2>
+    
     <?php if ( have_comments() ) : ?>
       <h2 class="comments__title">
-        <?php echo get_comments_number(); // Nombre de commentaires ?> Commentaire(s)
+        <?php
+          if (get_comments_number() > 1) {
+            echo get_comments_number() . ' commentaires';
+          }
+          else {
+            echo get_comments_number() . ' commentaire';
+          }
+        ?>
       </h2>
     
-      <ol class="comment__list">
+      <ul class="comment__list">
         <?php
           // La fonction qui liste les commentaires
           wp_list_comments( array(
-            'style'       => 'ol',
+            'style'       => 'ul',
             'short_ping'  => true,
-            // 'avatar_size' => 74,
+            'avatar_size' => 74,
           ) );
         ?>
-      </ol>
+      </ul>
         
       <!-- S'il n'y a pas de commentaires -->
       <?php else : ?>
-        <h2>Avis</h2>
-
         <p class="comments__none">Il n'y a pas de commentaires pour le moment. Soyez le premier à participer !</p>
     <?php endif; ?>
  
--- content/themes/maisonalibert/single.php
+++ content/themes/maisonalibert/single.php
@@ -1,5 +1,5 @@
 <!-- Product Details -->
 <?php get_header(); ?>
   <?php get_template_part('template-parts/products/details'); ?>
-  <?php get_template_part('template-parts/products/comments'); ?>
+  <?php get_template_part('template-parts/products/tpl-comments'); ?>
 <?php get_footer(); ?>
